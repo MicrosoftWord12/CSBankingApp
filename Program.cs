63562119d5b9dca14393a0522d48d10ed144d5dd
Microsft-Wrd <60941772+MicrosoftWord12@users.noreply.github.com>
2021-10-22 16:37:46 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSBankingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
