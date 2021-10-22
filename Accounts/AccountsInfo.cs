93dc6be70d91da35bc41725d439b0cf2e1ba7421
Microsft-Wrd <60941772+MicrosoftWord12@users.noreply.github.com>
2021-10-22 16:49:31 +0100

    AccountsInfo.cs - Utilities for banking system done, Program.cs - IDK it needed committing .gitignore - Ignoring .idea

---
+++ Accounts/AccountsInfo.cs
@@ -0,0 +1,73 @@
+﻿namespace CSBankingApp.Accounts
+{
+    public class AccountsInfo
+    {
+        private string nameF;
+        private string nameL;
+        private int accountNum;
+        private string email;
+        private string password;
+        private int pinCode;
+
+        public string GetNameF
+        {
+            get => nameF;
+        }
+
+        public string SetNameF
+        {
+            set => nameF = value;
+        }
+
+        public string GetNameL
+        {
+            get => nameL;
+        }
+
+        public string SetNameL
+        {
+            set => nameL = value;
+        }
+
+        public int GetAccountNum
+        {
+            get => accountNum;
+        }
+
+        public int SetAccountNum
+        {
+            set => accountNum = value;
+        }
+
+        public string GetEmail
+        {
+            get => email;
+        }
+
+        public string SetEmail
+        {
+            set => email = value;
+        }
+
+        public string GetPassword
+        {
+            get => password;
+        }
+
+        public string SetPassword
+        {
+            set => password = value;
+        }
+
+        public int GetPinCode
+        {
+            get => pinCode;
+        }
+
+        public int SetPinCode
+        {
+            set => pinCode = value;
+        }
+        
+    }
+}
